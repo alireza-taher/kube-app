a6e0133f7ace6b0089c6d8862f708f2058ea3f16
alireza <alireza.taher@icloud.com>
2025-03-14 16:41:35 +0400

    Add locust load-test

---
+++ load-test/locustfile.py
@@ -0,0 +1,25 @@
+import os
+import random
+import urllib3
+from locust import HttpUser, task, between
+
+# Disable warnings for self-signed SSL certificates
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+class CalculateUser(HttpUser):
+    host = "https://webapp.local"
+    wait_time = between(1, 3)
+
+    @task
+    def calculate(self):
+        api_key = os.environ.get("APIKEY", "")
+        headers = {"apikey": api_key}
+        param_value = random.randint(15, 30)
+
+        self.client.get(
+            "/calculate",
+            params={"param": param_value},
+            headers=headers,
+            verify=False
+        )
