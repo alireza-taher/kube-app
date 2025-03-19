5cbb5a7ed2fd79ba5f07f08e5099cb9a289e2426
alireza <alireza.taher@icloud.com>
2025-03-19 14:17:33 +0400

    Remove unnecessary command and ask user to enter privileged mode.

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+echo 'Please enter root password in order to stop LoadBalancer and restore original hosts'
+sudo -v
+
 # Stop cloud provider kind
 sudo pkill cloud-provider-kind
 
 # Remove custom entry from /etc/hosts
-EXTERNAL_IP=$(kubectl get service ingress-nginx-controller -o jsonpath='{.status.loadBalancer.ingress[0].ip}' -n ingress-nginx)
 sudo sed -i.backup "/webapp.local/d" /etc/hosts && sudo rm -f /etc/hosts.backup
 
 # Delete kind cluster
